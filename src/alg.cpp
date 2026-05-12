3095f88fbd36fdf4cb93baf0aad9194d0e907a7f
pavelsyzrantsev <pavelsyzrancev2007@gmail.com>
2026-05-12 19:22:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,18 +5,14 @@
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1]) {
+      continue;
+    }
     for (int j = i + 1; j < len; ++j) {
       if (arr[i] + arr[j] == value) {
         ++count;
-        int temp = arr[j];
-        while (j + 1 < len && arr[j + 1] == temp) {
-          ++j;
-        }
+        break;
       }
-    }
-    int temp = arr[i];
-    while (i + 1 < len && arr[i + 1] == temp) {
-      ++i;
     }
   }
   return count;
@@ -27,17 +23,13 @@ int countPairs2(int *arr, int len, int value) {
   int left = 0;
   int right = len - 1;
   while (left < right) {
+    if (left > 0 && arr[left] == arr[left - 1]) {
+      ++left;
+      continue;
+    }
     int sum = arr[left] + arr[right];
     if (sum == value) {
       ++count;
-      int left_val = arr[left];
-      int right_val = arr[right];
-      while (left + 1 < len && arr[left + 1] == left_val) {
-        ++left;
-      }
-      while (right - 1 >= 0 && arr[right - 1] == right_val) {
-        --right;
-      }
       ++left;
       --right;
     } else if (sum < value) {
@@ -49,7 +41,7 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 
-int binarySearch(int *arr, int left, int right, int target) {
+static int binarySearch(int *arr, int left, int right, int target) {
   while (left <= right) {
     int mid = left + (right - left) / 2;
     if (arr[mid] == target) {
@@ -66,22 +58,15 @@ int binarySearch(int *arr, int left, int right, int target) {
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1]) {
+      continue;
+    }
     int target = value - arr[i];
     if (target < arr[i]) {
-      break;
+      continue;
     }
-    int index = binarySearch(arr, i + 1, len - 1, target);
-    if (index != -1) {
+    if (binarySearch(arr, i + 1, len - 1, target) != -1) {
       ++count;
-      int temp = arr[index];
-      while (index + 1 < len && arr[index + 1] == temp) {
-        ++index;
-      }
-      i = index;
-    }
-    int temp = arr[i];
-    while (i + 1 < len && arr[i + 1] == temp) {
-      ++i;
     }
   }
   return count;
